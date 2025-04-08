7954ff1ee2098785d9aec522aee00bfb331ff430
吴俊 <1877002362@qq.com>
2025-04-08 18:12:09 +0800

    first commit

---
+++ src/main/java/com/keyi/data/rabbitmq_distributed_transactions/RabbitmqDistributedTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.keyi.data.rabbitmq_distributed_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDistributedTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDistributedTransactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keyi/data/rabbitmq_distributed_transactions/RabbitmqDistributedTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyi.data.rabbitmq_distributed_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDistributedTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
